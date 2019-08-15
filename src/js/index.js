eb7904e0cd7db809e55ac4c3fc41d55bfbde1da7
ysfscream <894402575@qq.com>
2019-08-15 23:23:35 +0800

    :lipstick: Add the darkmode

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,18 @@
 import '../assets/scss/style.scss'
 
+import Darkmode from 'darkmode-js'
 import { WiredButton, WiredIconButton, WiredCard, WiredTooltip, WiredProgress } from "wired-elements"
 import intro from './intro.js'
 import Time from './time.js'
+
+
+// Dark mode
+const options = {
+  label: '🌓',
+  autoMatchOsTheme: true,
+}
+const darkmode = new Darkmode(options)
+darkmode.showWidget()
 
 // JSON resume
 const introCardText = document.querySelector('#json')
@@ -11,6 +21,6 @@ screenWidth > 768
   ? introCardText.innerHTML = `<pre>${intro()}</pre>`
     : introCardText.innerHTML = intro()
 
-// time info
+// Time info
 Time.getTimeTitle()
 Time.progressOfYear()
